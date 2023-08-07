e5a95091d270a1f8b0b69e88800a70b869498808
Raphael Bethaz <bethazdev@protonmail.com>
2023-08-07 23:27:46 +0200

    first

---
+++ js/commands.js
@@ -0,0 +1,75 @@
+var twitter = "https://www.twitter.com/Izuchii07/";
+var password = "bethazdev";
+var linkedin = "https://www.linkedin.com/in/raphael-bethaz-6055991b6/";
+var github = "https://github.com/RBethaz/";
+var email = 'mailto:bethazdev@protonmail.com';
+
+whois = [
+    "<br>",
+    "Greetings, I'm Raphael, a 25-year-old software virtuoso residing in the scenic vicinity of Annecy, France.👋",
+    "With a fascination for the intricacies of back-end development and the intellectual art of machine learning, I set out on a journey filled with digital creation and innovation.",
+    "My odyssey commenced with comprehensive development training via OpenClassroom, laying the foundational stones of my craft.",
+    "Soon after, I collaborated with a cutting-edge cryptocurrency company, designing their virtual window to the world, and then architecting a dynamic web platform employing Django and React.",
+    "Fluent in the poetic languages of Python, JS, and Ruby, I weave code with both precision and passion.",
+    "Currently, I am immersed in my role as a software developer on Odoo for a leading web agency, a position that offers endless opportunities for exploration and growth.",
+    "Each day, I strive to push the boundaries of technology, motivated by a relentless pursuit of excellence and the thrill of the next big challenge.",
+    "My story is still being written, and I invite you to be a part of this exciting journey as I continue to shape the digital landscape.",
+    "<br>"
+  ];
+  
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/Izuchii07' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/Bethaz-Raphael' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/RBethaz' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Raphaël?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">credit</span>         Website made from ForrestKnight in Youtube ',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Bethaz Developpement.</span>',
+  
+"__________ ______________________  ___ ___     _____   __________ ________                          .__                                                              __   ",
+"\______   \\_   _____/\__    ___/ /   |   \   /  _  \  \____    / \______ \    ____  ___  __  ____  |  |    ____  ______  ______    ____    _____    ____    ____  _/  |_ ",
+"|    |  _/ |    __)_   |    |   /    ~    \ /  /_\  \   /     /   |    |  \ _/ __ \ \  \/ /_/ __ \ |  |   /  _ \ \____ \ \____ \ _/ __ \  /     \ _/ __ \  /    \ \   __\ ",
+"|    |   \ |        \  |    |   \    Y    //    |    \ /     /_   |    `   \\  ___/  \   / \  ___/ |  |__(  <_> )|  |_> >|  |_> >\  ___/ |  Y Y  \\  ___/ |   |  \ |  |  ",
+"|______  //_______  /  |____|    \___|_  / \____|__  //_______ \ /_______  / \___  >  \_/   \___  >|____/ \____/ |   __/ |   __/  \___  >|__|_|  / \___  >|___|  / |__|  ",
+"       \/         \/                   \/          \/         \/         \/      \/             \/               |__|    |__|         \/       \/      \/      \/      " ,
+
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
